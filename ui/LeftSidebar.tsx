f6264a45c892419c61187dec2a76e2cddb2f931b
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2024-01-30 11:33:55 +0600

    right navigation

--- ui/LeftSidebar.tsx
+++ ui/LeftSidebar.tsx
@@ -1,28 +1,60 @@
+"use client";
 import Link from "next/link";
 import { BsBook } from "react-icons/bs";
-import { FiLogOut } from "react-icons/fi";
+import { FiHome, FiLogOut } from "react-icons/fi";
 import { CiSettings, CiHeart } from "react-icons/ci";
-import Logo from "./Logo";
+import Logo from "@/ui/Logo";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
   return (
     <aside className="sidebar sidebar-left w-[70px] flex flex-col items-center ">
       <Logo />
       <div className="flex flex-col justify-between h-full w-full mt-6 py-5">
         <div className="left-sidebar-top w-full flex flex-col items-center  gap-5 justify-center">
-          <Link href="/">
-            <BsBook className="sidebar-icon" />
+          <Link
+            href="/"
+            className={cn({
+              active: pathname === "/",
+            })}
+          >
+            <FiHome className="w-6 h-6" />
           </Link>
-          <Link href="/">
-            <CiHeart className="sidebar-icon" />
+          <Link
+            href="/quran"
+            className={cn({
+              active: pathname === "/quran",
+            })}
+          >
+            <BsBook className="w-5 h-5" />
+          </Link>
+          <Link
+            href="/favorite"
+            className={cn({
+              active: pathname === "/favorite",
+            })}
+          >
+            <CiHeart className="w-7 h-7" />
           </Link>
         </div>
         <div className="left-sidebar-top w-full flex flex-col items-center justify-center gap-5">
-          <Link href="/">
-            <CiSettings className="sidebar-icon" />
+          <Link
+            href="/settings"
+            className={cn({
+              active: pathname === "/settings",
+            })}
+          >
+            <CiSettings className="w-7 h-7" />
           </Link>
-          <Link href="/">
-            <FiLogOut className="sidebar-icon" />
+          <Link
+            href="/logout"
+            className={cn({
+              active: pathname === "/logout",
+            })}
+          >
+            <FiLogOut className="w-5 h-5" />
           </Link>
         </div>
       </div>
--- ui/RightSidebar.tsx
+++ ui/RightSidebar.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import { FiUser } from "react-icons/fi";
+import { RxReader } from "react-icons/rx";
 const RightSidebar = () => {
   return (
-    <aside className="sidebar sidebar-right w-44 px-6 pt-4">
-      <div>right</div>
+    <aside className="sidebar sidebar-right lg:w-52 w-48 px-4 py-4 mt-[1px]">
+      <div className="flex flex-col justify-between h-full">
+        <div>
+          <div className="flex justify-between flex-row-reverse gap-3 items-center border-b pb-6">
+            <Avatar>
+              <AvatarImage src="" alt="" />
+              <AvatarFallback>
+                <FiUser />
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <span className="text-xs  font-normal">Salamu Alaykum,</span>
+              <span className="font-bold">MH</span>
+            </div>
+          </div>
+          <div className="flex justify-between flex-row-reverse gap-3 items-start py-6">
+            <RxReader className="w-8 h-8" />
+            <div className="flex flex-col gap-1">
+              <span className="text-[8px] text-primary font-semibold uppercase">
+                Last Read
+              </span>
+              <span className="text-[10px] font-bold">Al-Fatiah</span>
+              <span className="text-[8px]">Ayath no: 1</span>
+            </div>
+          </div>
+
+        </div>
+          <Card className="text-[8px] bg-primary text-white shadow-none">
+            <CardContent className="p-4 flex flex-col gap-4">
+              <CardTitle className="text-muted">Ayath of the day</CardTitle>
+              <p className="space-y-4">
+                Glory be to the One Who took His servant ˹Muḥammad˺ by night
+                from the Sacred Mosque to the Farthest Mosque whose surroundings
+                We have blessed, so that We may show him some of Our signs. 1
+                Indeed, He 2 alone is the All-Hearing, All-Seeing.
+              </p>
+              <CardFooter className="p-0">
+                <Link href="#" className="hover:text-muted active:text-muted">
+                  Read More
+                </Link>
+              </CardFooter>
+            </CardContent>
+          </Card>
+      </div>
     </aside>
   );
-}
+};
 
-export default RightSidebar
+export default RightSidebar;
