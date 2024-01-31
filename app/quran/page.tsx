c13257dafd9c3bf4e24bf241a40b63ee9a976df3
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2024-01-31 16:52:54 +0600

    updated page

--- app/quran/page.tsx
+++ app/quran/page.tsx
@@ -6,12 +6,12 @@ const QuranPage = () => {
     <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 w-full gap-3">
       {surahData.map((surah) => (
         <Card
-          className="h-24 shadow-none hover:shadow-md transition-shadow cursor-pointer bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#fafafa] to-[#fbfbfb]"
+          className="group h-24 shadow-none hover:shadow-md transition-shadow cursor-pointer bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#fafafa] to-[#fbfbfb]"
           key={surah.number}
         >
           <CardContent className="p-2 flex flex-col justify-center h-full">
             <CardTitle className="mb-4">
-              <Button className="rounded-full text-[8px] bg-muted text-primary hover:text-muted h-5 w-5 p-2">
+              <Button className="rounded-full text-[8px] bg-muted text-primary group-hover:bg-primary group-hover:text-muted h-5 w-5 p-2">
                 {surah.number}
               </Button>
             </CardTitle>
